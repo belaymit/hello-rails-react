4144fba0a3a8568cba05454379dd851ac92fe4a3
belaymit <2belamit@gmail.com>
2023-05-01 20:59:46 +0300

    Created databased seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello, Rails React!")
+Greeting.create(message: "Hello, this Belay, full stack student at Microverse")
+Greeting.create(message: "Hello, We are devloping rails app here")
+Greeting.create(message: "Good mornig, We are working on our final capstone project")
+Greeting.create(message: "Good evening from Belay!")
