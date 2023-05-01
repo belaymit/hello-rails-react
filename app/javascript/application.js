53ec6dccfdc110b349f025e9ed75665ec98626dc
belaymit <2belamit@gmail.com>
2023-05-01 21:10:50 +0300

    Created the main entry point to reactjs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import App from "./react/src/components/App"
+import store from './react/src/redux/configureStore'
+import { fetchGreeting } from './react/src/redux/greeting/greetingSlice'
+store.dispatch(fetchGreeting())
+
+ReactDOM.render (
+  <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
